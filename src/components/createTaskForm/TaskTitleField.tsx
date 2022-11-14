5d3a8189958a3e0483cb6f24b45ea7fce52a0d27
al-jabir <aljabir098@gmail.com>
2022-11-14 20:36:17 +0600

    taskTitleField components done

--- src/components/createTaskForm/TaskTitleField.tsx
+++ src/components/createTaskForm/TaskTitleField.tsx
@@ -1,7 +1,12 @@
 import { TextField } from '@mui/material';
 import { FC, ReactElement } from 'react';
+import { ITextField } from './interfaces/ITextField';
 
-export const TaskTitleField: FC = (): ReactElement => {
+import PropTypes from 'prop-types';
+
+export const TaskTitleField: FC<ITextField> = (props): ReactElement => {
+  const { onChange = (e) => console.log(e), disabled = false } = props;
+
   return (
     <TextField
       id='title'
@@ -11,8 +16,13 @@ export const TaskTitleField: FC = (): ReactElement => {
       size='small'
       name='title'
       fullWidth
-      disabled={false}
-      onChange={(e) => console.log(e.target.value)}
+      disabled={disabled}
+      onChange={onChange}
     />
   );
+};
+
+TaskTitleField.propTypes = {
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool,
 };
