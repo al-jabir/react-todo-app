fdd9edebadd9f8e3b62e01a12dddd03cc6e3ee66
al-jabir <aljabir098@gmail.com>
2022-11-14 20:14:24 +0600

    I have done components

--- src/components/createTaskForm/TaskTitleField.tsx
+++ src/components/createTaskForm/TaskTitleField.tsx
@@ -1,5 +1,18 @@
-const TaskTitleField = () => {
-  return <div>TaskTitleField</div>;
-};
+import { TextField } from '@mui/material';
+import { FC, ReactElement } from 'react';
 
-export default TaskTitleField;
+export const TaskTitleField: FC = (): ReactElement => {
+  return (
+    <TextField
+      id='title'
+      label='Task Title'
+      placeholder='Task Title'
+      variant='outlined'
+      size='small'
+      name='title'
+      fullWidth
+      disabled={false}
+      onChange={(e) => console.log(e.target.value)}
+    />
+  );
+};
