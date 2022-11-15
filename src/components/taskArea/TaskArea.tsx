b2a7341bd179e2caeca6fee0b4d71989409f0834
al-jabir <aljabir098@gmail.com>
2022-11-15 11:33:38 +0600

    break

--- src/components/taskArea/TaskArea.tsx
+++ src/components/taskArea/TaskArea.tsx
@@ -1,10 +1,33 @@
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import { FC, ReactElement } from 'react';
+
+import { format } from 'date-fns';
+import TaskCounter from '../taskCounter/TaskCounter';
 
 const TaskArea: FC = (): ReactElement => {
   return (
     <Grid item md={8} px={4}>
-      <h2>Content Area</h2>
+      <Box mb={8} px={4}>
+        <h2>Status Of Your Tasks As on {format(new Date(), 'PPPP')}</h2>
+      </Box>
+      <Grid container display='flex' justifyContent='center'>
+        <Grid
+          item
+          display='flex'
+          justifyContent='space-between'
+          alignItems='center'
+          md={10}
+          xs={12}
+          mb={8}>
+          <TaskCounter />
+          <TaskCounter />
+          <TaskCounter />
+        </Grid>
+        <Grid item display='flex' flexDirection='column'>
+          <Box>Tasks Will Come Over Here</Box>
+          <Box>Tasks Will Come Over Here</Box>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
--- src/components/taskCounter/TaskCounter.tsx
+++ src/components/taskCounter/TaskCounter.tsx
@@ -1,5 +1,49 @@
-const TaskCounter = () => {
-  return <div>TaskCounter</div>;
+import { Avatar, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { FC, ReactElement } from 'react';
+
+import { Status } from '../createTaskForm/enums/Status';
+import { ItaskCounter } from './interface/ItaskCounter';
+
+import PropTypes from 'prop-types';
+
+const TaskCounter: FC<ItaskCounter> = (props): ReactElement => {
+  const { status = Status.completed, count = 0 } = props;
+  return (
+    <>
+      <Box
+        display='flex'
+        flexDirection='column'
+        justifyContent='center'
+        alignItems='center'>
+        <Avatar
+          sx={{
+            backgroundColor: 'transparent',
+            border: '3px solid',
+            width: '90px',
+            height: '90px',
+            marginBottom: '16px',
+            borderColor: 'warning.light',
+          }}>
+          <Typography color='#ffffff' variant='h4'>
+            {count}
+          </Typography>
+        </Avatar>
+        <Typography
+          color='#ffffff'
+          fontWeight='bold'
+          fontSize='20px'
+          variant='h5'>
+          {status}
+        </Typography>
+      </Box>
+    </>
+  );
 };
 
 export default TaskCounter;
+
+TaskCounter.propTypes = {
+  count: PropTypes.number,
+  status: PropTypes.oneOf([Status.todo, Status.inProgress, Status.completed]),
+};
