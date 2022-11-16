1bc7ff4170e50cfc53edc226dee2fbf576afe650
al-jabir <aljabir098@gmail.com>
2022-11-16 13:29:05 +0600

    done

--- src/components/task/TaskFooter.tsx
+++ src/components/task/TaskFooter.tsx
@@ -1,5 +1,40 @@
-const TaskFooter = () => {
-  return <div>TaskFooter</div>;
+import { Box, Button, FormControlLabel, Switch } from '@mui/material';
+import { FC, ReactElement } from 'react';
+
+import { ITaskFooter } from './interface/ITaskFooter';
+
+import PropTypes from 'prop-types';
+
+export const TaskFooter: FC<ITaskFooter> = (props): ReactElement => {
+  //  Destructure props
+  const {
+    onStatusChange = (e) => console.log(e),
+    onClick = (e) => console.log(e),
+  } = props;
+
+  return (
+    <Box
+      display='flex'
+      justifyContent='space-between'
+      alignItems='center'
+      mt={4}>
+      <FormControlLabel
+        label='In Progress'
+        control={<Switch onChange={(e) => onStatusChange(e)} color='warning' />}
+      />
+      <Button
+        variant='contained'
+        color='success'
+        size='small'
+        sx={{ color: '#ffffff' }}
+        onClick={(e) => onClick(e)}>
+        Mark Complete
+      </Button>
+    </Box>
+  );
 };
 
-export default TaskFooter;
+TaskFooter.propTypes = {
+  onStatusChange: PropTypes.func,
+  onClick: PropTypes.func,
+};
