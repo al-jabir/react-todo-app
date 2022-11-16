404fdb21ceb2b5dc45ec25ef712e42ce50401c56
al-jabir <aljabir098@gmail.com>
2022-11-16 12:35:12 +0600

    TaskDescription done

--- src/components/task/TaskDescription.tsx
+++ src/components/task/TaskDescription.tsx
@@ -1,5 +1,20 @@
-const TaskDescription = () => {
-  return <div>TaskDescription</div>;
+import { Box, Typography } from '@mui/material';
+import { FC, ReactElement } from 'react';
+
+import PropTypes from 'prop-types';
+import { ITaskDescription } from './interface/ITaskDescription';
+
+export const TaskDescription: FC<ITaskDescription> = (props): ReactElement => {
+  //  Destructure Props
+  const { description = 'Lorem ipsum dolor sit amet' } = props;
+
+  return (
+    <Box>
+      <Typography>{description}</Typography>
+    </Box>
+  );
 };
 
-export default TaskDescription;
+TaskDescription.propTypes = {
+  description: PropTypes.string,
+};
