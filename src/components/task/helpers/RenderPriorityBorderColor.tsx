1adcfef2a05e033a47caca6d9ab9a5754835d1bf
al-jabir <aljabir098@gmail.com>
2022-11-16 11:12:34 +0600

    renderPriorityBorderColor

--- src/components/task/helpers/RenderPriorityBorderColor.tsx
+++ src/components/task/helpers/RenderPriorityBorderColor.tsx
@@ -1,5 +1,14 @@
-const RenderPriorityBorderColor = () => {
-  return <div>RenderPriorityBorderColor</div>;
-};
+import { Priority } from '../../createTaskForm/enums/Priority';
 
-export default RenderPriorityBorderColor;
+export const renderPriorityBorderColor = (priority: string): string => {
+  switch (priority) {
+    case Priority.normal:
+      return 'grey.900';
+    case Priority.low:
+      return 'info.light';
+    case Priority.high:
+      return 'error.light';
+    default:
+      return 'grey.900';
+  }
+};
