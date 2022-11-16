ddd651fecbb3cc7b385f1a166c7e1d6d81f0003a
al-jabir <aljabir098@gmail.com>
2022-11-16 11:51:44 +0600

    TaskHeader

--- src/components/task/TaskHeader.tsx
+++ src/components/task/TaskHeader.tsx
@@ -1,5 +1,28 @@
-const TaskHeader = () => {
-  return <div>TaskHeader</div>;
+import { Box, Chip, Typography } from '@mui/material';
+import { FC, ReactElement } from 'react';
+
+import { ITaskHeader } from './interface/ITaskHeader';
+
+import { format } from 'date-fns';
+import PropTypes from 'prop-types';
+
+export const TaskHeader: FC<ITaskHeader> = (props): ReactElement => {
+  // Destructure props
+  const { title = 'Default Title', date = new Date() } = props;
+
+  return (
+    <Box display='flex' width='100%' justifyContent='space-between' mb={3}>
+      <Box>
+        <Typography variant='h6'>{title}</Typography>
+      </Box>
+      <Box>
+        <Chip variant='outlined' label={format(date, 'PPP')} />
+      </Box>
+    </Box>
+  );
 };
 
-export default TaskHeader;
+TaskHeader.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.instanceOf(Date),
+};
